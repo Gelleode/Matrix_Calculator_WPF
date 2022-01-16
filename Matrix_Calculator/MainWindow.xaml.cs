0e0438b21361cda00c689ca564f9e304940eb242
Danila Solntsev <danilas444@gmail.com>
2022-01-16 22:48:41 +0300

    Add project files.

---
+++ Matrix_Calculator/MainWindow.xaml.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Matrix_Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public dynamic Read_Matrix(string Matrix)
+        {
+            Matrix = Matrix.Replace("\r", "");
+            string[] Rows = Matrix.Split('\n');
+            int Row = Rows.Length;
+            int Column = Rows[0].Split(' ').Length;
+            Matrix A = new Matrix(Row, Column);
+            foreach (string a in Rows)
+                if (a.Split(' ').Length != Column)
+                    return "Wrong input";
+            for (int i = 0; i < Rows.Length; i++)
+            {
+                string[] s = Rows[i].Split(' ');
+                for (int j = 0; j < Column; j++)
+                    A.matrix[i, j] = Convert.ToInt32(s[j]);
+            }
+
+            return A;
+        }
+
+        private void Add_Btn(object sender, RoutedEventArgs e)
+        {
+            Matrix_Algebra_TBox.Text = "";
+            if (string.IsNullOrEmpty(Matrix_A_TBox.Text) || string.IsNullOrEmpty(Matrix_B_TBox.Text))
+            {
+                Matrix_Result_TBox.Text = "No input";
+                return;
+            }
+            var A = Read_Matrix(Matrix_A_TBox.Text);
+            var B = Read_Matrix(Matrix_B_TBox.Text);
+            if (A.GetType() == typeof(string))
+            {
+                Matrix_Result_TBox.Text = A.ToString();
+                return;
+            }
+            if (B.GetType() == typeof(string))
+            {
+                Matrix_Result_TBox.Text = B.ToString();
+                return;
+            }
+
+            if (A.GetType() == typeof(Matrix) & B.GetType() == typeof(Matrix))
+            {
+                Matrix C = A + B;
+                Matrix_Result_TBox.Text = C.ToString();
+                string calculations = null;
+                for (int i = 0; i < C.CountRow; i++)
+                    for (int j = 0; j < C.CountColumn; j++)
+                        calculations += $"C[{i},{j}] = {A[i, j]} + {B[i, j]} = {C[i, j]}\n";
+                Matrix_Algebra_TBox.Text = calculations;
+            }
+        }
+
+        private void Subtract_Btn(object sender, RoutedEventArgs e)
+        {
+            Matrix_Algebra_TBox.Text = "";
+            if (string.IsNullOrEmpty(Matrix_A_TBox.Text) || string.IsNullOrEmpty(Matrix_B_TBox.Text))
+            {
+                Matrix_Result_TBox.Text = "No input";
+                return;
+            }
+            var A = Read_Matrix(Matrix_A_TBox.Text);
+            var B = Read_Matrix(Matrix_B_TBox.Text);
+            if (A.GetType() == typeof(string))
+            {
+                Matrix_Result_TBox.Text = A.ToString();
+                return;
+            }
+            if (B.GetType() == typeof(string))
+            {
+                Matrix_Result_TBox.Text = B.ToString();
+                return;
+            }
+
+            if (A.GetType() == typeof(Matrix) & B.GetType() == typeof(Matrix))
+            {
+                Matrix C = A - B;
+                Matrix_Result_TBox.Text = C.ToString();
+                string calculations = null;
+                for (int i = 0; i < C.CountRow; i++)
+                    for (int j = 0; j < C.CountColumn; j++)
+                        calculations += $"C[{i},{j}] = {A[i,j]} - {B[i,j]} = {C[i, j]}\n";
+                Matrix_Algebra_TBox.Text = calculations;
+            }
+        }
+
+        private void Multiply_Btn(object sender, RoutedEventArgs e)
+        {
+            Matrix_Algebra_TBox.Text = "";
+            if (string.IsNullOrEmpty(Matrix_A_TBox.Text) || string.IsNullOrEmpty(Matrix_B_TBox.Text))
+            {
+                Matrix_Result_TBox.Text = "No input";
+                return;
+            }
+            var A = Read_Matrix(Matrix_A_TBox.Text);
+            var B = Read_Matrix(Matrix_B_TBox.Text);
+            if (A.GetType() == typeof(string))
+            {
+                Matrix_Result_TBox.Text = A.ToString();
+                return;
+            }
+            if (B.GetType() == typeof(string))
+            {
+                Matrix_Result_TBox.Text = B.ToString();
+                return;
+            }
+
+            if (A.GetType() == typeof(Matrix) & B.GetType() == typeof(Matrix))
+            {
+                Matrix C = A * B;
+                Matrix_Result_TBox.Text = C.ToString();
+                string calculations = null;
+                for (int i = 0; i < C.CountRow; i++)
+                {
+                    for (int j = 0; j < C.CountColumn; j++)
+                    {
+                        calculations += $"C[{i},{j}] = ";
+                        for (int k = 0; k < B.CountRow; k++)
+                        {
+                            if (k == 0)
+                                calculations += $"{A[i, k]} * {B[k, j]} ";
+                            else
+                                calculations += $"+ {A[i, k]} * {B[k, j]} ";
+                        }
+                        calculations += $"= {C[i, j]}\n";
+                    }
+                }
+                Matrix_Algebra_TBox.Text = calculations;
+            }
+        }
+        
+        private void Transpose_Btn(object sender, RoutedEventArgs e)
+        {
+            Matrix_Algebra_TBox.Text = "";
+            if (string.IsNullOrEmpty(Matrix_A_TBox.Text))
+            {
+                Matrix_Result_TBox.Text = "No input";
+                return;
+            }
+            var A = Read_Matrix(Matrix_A_TBox.Text);
+            if (A.GetType() == typeof(string))
+            {
+                Matrix_Result_TBox.Text = A.ToString();
+                return;
+            }
+
+            Matrix_Result_TBox.Text = Matrix.Transponse(A).ToString();
+        }
+    }
+}
---
+++ Matrix_Calculator/Matrix.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Text;
+
+namespace Matrix_Calculator
+{
+    public class Matrix
+    {
+        public float[,] matrix = null;
+
+        public int CountColumn { get; private set; }
+        public int CountRow { get; private set; }
+
+        public Matrix(int x = 1, int y = 1)
+        {
+            matrix = new float[x, y];
+
+            CountColumn = y;
+            CountRow = x;
+        }
+
+        public float this[int x, int y]
+        {
+            get { return matrix[x, y]; }
+            set { matrix[x, y] = value; }
+        }
+        public override string ToString()
+        {
+            StringBuilder ret = new StringBuilder();
+            if (matrix == null) return ret.ToString();
+
+            for (int i = 0; i < CountRow; i++)
+            {
+                for (int t = 0; t < CountColumn; t++)
+                {
+                    ret.Append(matrix[i, t]);
+                    ret.Append(" ");
+                }
+                ret.Append("\n");
+            }
+            return ret.ToString();
+        }
+
+        public static dynamic Sum(Matrix A, Matrix B)
+        {
+            if (A.CountColumn == B.CountColumn & A.CountRow == B.CountRow)
+            {
+                Matrix C = new Matrix(A.CountRow, A.CountColumn);
+                for (int i = 0; i < C.CountRow; i++)
+                    for (int j = 0; j < C.CountColumn; j++)
+                        C[i, j] = A[i, j] + B[i, j];
+                return C;
+            }
+            else return "Can't add these matrices";
+        }
+
+        public static dynamic Subtract(Matrix A, Matrix B)
+        {
+            if (A.CountColumn == B.CountColumn & A.CountRow == B.CountRow)
+            {
+                Matrix C = new Matrix(A.CountRow, A.CountColumn);
+                for (int i = 0; i < C.CountRow; i++)
+                    for (int j = 0; j < C.CountColumn; j++)
+                        C[i, j] = A[i, j] - B[i, j];
+                return C;
+            }
+            else return "Can't subtract these matrices";
+        }
+
+        public static dynamic Multiply(Matrix A, Matrix B)
+        {
+            if (A.CountColumn == B.CountRow)
+            {
+                Matrix C = new Matrix(A.CountRow, B.CountColumn);
+                for (int i = 0; i < C.CountRow; i++)
+                    for (int j = 0; j < C.CountColumn; j++)
+                        for (int k = 0; k < B.CountRow; k++)
+                            C[i, j] += A[i, k] * B[k, j];
+                return C;
+            }
+            else return "Can't multiply these matrices";
+        }
+
+        public static Matrix MultiplyByNumber(Matrix A, int b)
+        {
+            Matrix B = new Matrix(A.CountRow, A.CountColumn);
+            for (int i = 0; i < A.CountRow; i++)
+                for (int j = 0; j < A.CountColumn; j++)
+                    B[i, j] = A[i, j] * b;
+            return B;
+        }
+
+        public static Matrix Transponse(Matrix A)
+        {
+            Matrix B = new Matrix(A.CountColumn, A.CountRow);
+            for (int i = 0; i < B.CountRow; i++)
+                for (int j = 0; j < B.CountColumn; j++)
+                    B[i, j] = A[j, i];
+            return B;
+        }
+
+        public static dynamic operator *(Matrix A, Matrix B)
+        {
+            return Multiply(A, B);
+        }
+
+        public static dynamic operator *(Matrix A, int b)
+        {
+            return MultiplyByNumber(A, b);
+        }
+
+        public static dynamic operator +(Matrix A, Matrix B)
+        {
+            return Sum(A, B);
+        }
+
+        public static dynamic operator -(Matrix A, Matrix B)
+        {
+            return Subtract(A, B);
+        }
+    }
+}
+
